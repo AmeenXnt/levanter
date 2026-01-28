1cce29cdf4d71d1b7015dfe532022f20977f8489
AmeenXnt <ameenserbots@gmail.com>
2026-01-28 23:20:00 +0530

    Andi

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const DATABASE_URL =
   process.env.DATABASE_URL === undefined ? databasePath : process.env.DATABASE_URL
 module.exports = {
   VERSION: require('./package.json').version,
-  SESSION_ID: (process.env.SESSION_ID || '').trim(),
+  SESSION_ID: (process.env.SESSION_ID || 'levanter_28254551e408ec4312963e50dd2dbebc72').trim(),
   DATABASE:
     DATABASE_URL === databasePath
       ? new Sequelize({
